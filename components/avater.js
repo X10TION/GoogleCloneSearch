2da031a5ec9439f9ada3ae635d0f39b9013f63c6
Christopher White <x0tion007@gmail.com>
2022-09-24 17:05:01 +0100

    initial commit

---
+++ components/avater.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Avater({url}) {
+  return (
+    <img loading='lazy'
+    src={url}  
+    alt="Profile picture"
+    className='h-10 rounded-full cursor-pointer transition duration-150 transform hover:scale-110'/>
+  )
+}
