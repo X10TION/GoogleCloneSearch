328fff85ab88106c582178cf889543d5d60f38e7
Christopher White <x0tion007@gmail.com>
2022-09-26 15:22:56 +0100

    image commponet

--- components/avater.js
+++ components/avater.js
@@ -1,8 +1,8 @@
 import React from 'react'
-
+import Image from 'next/image'
 export default function Avater({url, className}) {
   return (
-    <img loading='lazy'
+    <Image loading='lazy'
     src={url}  
     alt="Profile picture"
     className={`h-10 rounded-full cursor-pointer transition duration-150 transform hover:scale-110 ${className}`}/>
